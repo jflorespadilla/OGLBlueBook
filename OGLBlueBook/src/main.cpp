48cefd6661ed717587ea880f135e84dd54b30cbb
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2022-08-18 03:25:54 -0700

    Got glew window to work.

--- OGLBlueBook/src/main.cpp
+++ OGLBlueBook/src/main.cpp
@@ -1,13 +1,15 @@
-#include <GL/glew.h>
-#include <GL/glut.h>
-...
-glutInit(&argc, argv);
-glutCreateWindow("GLEW Test");
-GLenum err = glewInit();
-if (GLEW_OK != err)
-{
-	/* Problem: glewInit failed, something is seriously wrong. */
-	fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-	...
+#include <gl/glew.h>
+#include <gl/glut.h>
+#include <stdio.h>
+
+
+int main(int argv, char **argc) {
+	glutInit(&argv, argc);
+	glutCreateWindow("GLEW Test");
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		/* Problem: glewInit failed, something is seriously wrong. */
+		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+	}
+	fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
 }
-fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
