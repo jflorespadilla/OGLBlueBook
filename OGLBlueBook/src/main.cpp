c89448301107d9108429682c188bb452581908ca
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2022-08-25 00:21:30 -0700

    Got a red screen going.

--- OGLBlueBook/src/main.cpp
+++ OGLBlueBook/src/main.cpp
@@ -2,10 +2,20 @@
 #include <gl/glut.h>
 #include <stdio.h>
 
+static void RenderSceneCB() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
 
 int main(int argv, char **argc) {
 	glutInit(&argv, argc);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowSize(1024, 768);
+	glutInitWindowPosition(100, 100);
+
 	glutCreateWindow("GLEW Test");
+
+	glutDisplayFunc(RenderSceneCB);
 	GLenum err = glewInit();
 	if (GLEW_OK != err) {
 		/* Problem: glewInit failed, something is seriously wrong. */
@@ -13,9 +23,7 @@ int main(int argv, char **argc) {
 	}
 	fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
 
-	static const GLfloat red[] = { 1.0f, 0.0f, 0.0f, 0.0f, };
-
-	while (1) {
-		glClearBufferfv(GL_COLOR, 0, red);
-	}
+	GLclampf red = 1.0f, green = 0.0f, blue = 0.0f, alpha = 0.0f;
+	glClearColor(red, green, blue, alpha);
+	glutMainLoop();
 }
