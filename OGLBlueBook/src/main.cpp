e595fc55dee2090d9cd3d52fc6b2c6153a487d61
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2022-08-25 23:40:19 -0700

    Switched over to using GLFW. The docs are better and they might make things a little eastier to work with.

--- OGLBlueBook/src/main.cpp
+++ OGLBlueBook/src/main.cpp
@@ -1,29 +1,37 @@
-#include <gl/glew.h>
-#include <gl/glut.h>
-#include <stdio.h>
+#include <GLFW/glfw3.h>
 
-static void RenderSceneCB() {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glutSwapBuffers();
-}
+int main() {
+    GLFWwindow* window;
 
-int main(int argv, char **argc) {
-	glutInit(&argv, argc);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-	glutInitWindowSize(1024, 768);
-	glutInitWindowPosition(100, 100);
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
 
-	glutCreateWindow("GLEW Test");
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
 
-	glutDisplayFunc(RenderSceneCB);
-	GLenum err = glewInit();
-	if (GLEW_OK != err) {
-		/* Problem: glewInit failed, something is seriously wrong. */
-		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-	}
-	fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
 
-	GLclampf red = 1.0f, green = 0.0f, blue = 0.0f, alpha = 0.0f;
-	glClearColor(red, green, blue, alpha);
-	glutMainLoop();
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(1.0f, 0.5f, 0.5f, 0.0f);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
