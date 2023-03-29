41effa7008776552ed23d4b755e5644695f128b7
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2023-03-29 00:12:20 -0700

    Added window hints to window creation.

--- OGLBlueBook/classes/GFXManager.cpp
+++ OGLBlueBook/classes/GFXManager.cpp
@@ -10,6 +10,10 @@ m_glfwFlag(false)
         m_glfwFlag = true;
     }
 
+    // Adding window hints for clarity
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     m_window = glfwCreateWindow(640, 480, "Hola", NULL, NULL);
     if (!m_window) {
         glfwTerminate();
