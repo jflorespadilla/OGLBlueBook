44d75b756d534d293070c3e9bbd9c51ee88d6765
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2023-04-04 23:03:42 -0700

    Started work on Shader class. Still trying to decided on the final design of the class.

--- OGLBlueBook/classes/Shader.cpp
+++ OGLBlueBook/classes/Shader.cpp
@@ -1,1 +1,17 @@
 #include "Shader.h"
+
+Shader::Shader() {
+
+}
+
+Shader::~Shader() {
+
+}
+
+void Shader::CreateShader(std::string FilePath) {
+	// Stub
+}
+
+void Shader::CompileShaders() {
+	// Stub
+}
--- OGLBlueBook/classes/Shader.h
+++ OGLBlueBook/classes/Shader.h
@@ -3,13 +3,23 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <vector>
+#include <gl/glew.h>
+
+enum class ShaderType {
+	NONE = -1, VERTEX = 0, FRAGMENT = 1, TC = 2, T = 3, G = 4 
+};
 
 class Shader
 {
 public:
-
+	Shader();
+	~Shader();
+	void CreateShader(std::string FilePath);
+	void CompileShaders();
 
 private:
-
+	std::vector<std::string> ShaderSourceList;
+	std::vector<GLuint> CompiledShaders;
 };
 
