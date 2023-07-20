03d237ea3de25929b1a7e4edf0393e624f9f32d5
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2023-07-19 21:04:55 -0700

    Creating ModelObject class.

---
+++ OGLBlueBook/ModelObject.h
@@ -0,0 +1,17 @@
+#pragma once
+// Going to replicate a similar class found in the sb7 source
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
+class ModelObject
+{
+	// Load object via file
+	// Destrcutor
+
+	// Will need to comeback in implement this afte reading appendix section.
+
+};
+
