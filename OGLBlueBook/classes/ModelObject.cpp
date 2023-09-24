9ce49a1e0f64eed333a373243989acbc30e38cda
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2023-09-23 20:49:00 -0700

    Doing adjustments to help organize my thoughts.

--- OGLBlueBook/classes/ModelObject.cpp
+++ OGLBlueBook/classes/ModelObject.cpp
@@ -1,15 +1,14 @@
 #include "ModelObject.h"
 
-ModelObject::~ModelObject() {
-	// Set flat for no loaded file
-	// Prompt user to provide a file string
-}
-
 ModelObject::ModelObject(const char* file, ModelType modelType) {
 	// Look at the model type
 	// Load up the file using the appropriate loader
 }
 
+ModelObject::~ModelObject() {
+	// Set flat for no loaded file
+	// Prompt user to provide a file string
+}
 
 void ModelObject::loadOBJModel(const std::fstream& ModelFile) {
 	// Parce file
@@ -35,24 +34,4 @@ void ModelObject::loadModel(const char* file, ModelType modelType) {
 		std::cerr << importer.GetErrorString();
 		return;
 	}
-	/*std::fstream ModelFile(file, std::fstream::in);
-	
-	if (!ModelFile.is_open()) {
-		return;
-	}*/
-
-	// Hopefully this glorified switch statement saves me some work later...
-
-	/*switch (modelType) {
-	case ModelType::OBJ:
-		loadOBJModel(ModelFile);
-		break;
-	case ModelType::STL:
-		loadSTLModel(ModelFile);
-		break;
-	case ModelType::VRML:
-		loadVRML(ModelFile);
-		break;
-	}
-	ModelFile.close();*/
 }
