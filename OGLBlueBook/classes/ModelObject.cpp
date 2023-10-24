6084ab22c68a39c059daca7975eedc9b615afb56
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2023-10-23 23:39:13 -0700

    Rethinking my ModelObject class. Need to build it organically.

--- OGLBlueBook/classes/ModelObject.cpp
+++ OGLBlueBook/classes/ModelObject.cpp
@@ -1,37 +1,9 @@
 #include "ModelObject.h"
 
-ModelObject::ModelObject(const char* file, ModelType modelType) {
-	// Look at the model type
-	// Load up the file using the appropriate loader
+ModelObject::ModelObject() {
+	std::cout << "Model object created" << std::endl;
 }
 
 ModelObject::~ModelObject() {
-	// Set flat for no loaded file
-	// Prompt user to provide a file string
-}
-
-void ModelObject::loadOBJModel(const std::fstream& ModelFile) {
-	// Parce file
-	// Load data
-}
-
-void ModelObject::loadSTLModel(const std::fstream& ModelFile) {
-	// Parce file
-	// Load data
-}
-
-void ModelObject::loadVRML(const std::fstream& ModelFile) {
-	// Parce file
-	// Load data
-}
-
-void ModelObject::loadModel(const char* file, ModelType modelType) {
-
-	Assimp::Importer importer;
-	const aiScene* scene = importer.ReadFile(file, aiComponent_TEXCOORDS | aiComponent_COLORS);
-
-	if (nullptr == scene) {
-		std::cerr << importer.GetErrorString();
-		return;
-	}
+	std::cout << "Model object destroyed" << std::endl;
 }
--- OGLBlueBook/classes/ModelObject.h
+++ OGLBlueBook/classes/ModelObject.h
@@ -1,11 +1,11 @@
 #pragma once
-// Going to replicate a similar class found in the sb7 source
+
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <fstream>
 #include <vector>
-
+// Need to learn how to use this library properly
 #include <assimp/Importer.hpp> 
 
 // Add model type formats as necessary
@@ -15,16 +15,8 @@ class ModelObject
 {
 	public:
 		ModelObject();
-		ModelObject(const char* file, ModelType modelType);
-		ModelObject(const ModelObject &modelObject);
 		~ModelObject();
-		void loadSTLModel(const std::fstream& ModelFile);
-		void loadOBJModel(const std::fstream& ModelFile);
-		void loadVRML(const std::fstream& ModelFile);
-		void loadModel(const char* file, ModelType modelType);
 
 	private:
-		// u,v coords?
-		// positional vectors?
 };
 
